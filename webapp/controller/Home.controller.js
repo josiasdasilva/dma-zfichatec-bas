2730a710c0ab0e3ed67245305f66884315b9eb13
Mauricio da Silva <mauricio.da.silva@gmail.com>
2021-04-08 19:08:14 +0000

    Implementações: 1.1 - ToggleButtons de 'Totalizador' e 'Bandeira' foram alterados para Checkbox; 1.2 - Reimplementada a lógica para tratar os Checkboxes; 1.3 - Agora ao pesquisar, se os dois Checkboxes de 'Totalizador' não estiverem selecionados, seleciona ambos automaticamente; 1.4 - Ajustado o MultiInput de Fornecedor para impossibilitar a digitação de texto/número.

--- webapp/controller/Home.controller.js
+++ webapp/controller/Home.controller.js
@@ -103,18 +103,38 @@ sap.ui.define([
 
             // Validação do CheckBox "Bandeira"
             if(this.getBandeiraSelectedIndex() === -1){
+                this.getView().byId("idCheckBoxAtacado1").setSelected(true);
+                this.getView().byId("idCheckBoxVarejo1").setSelected(true);
+            }
+
+/*
+            // Validação do CheckBox "Bandeira"
+            if(this.getBandeiraSelectedIndex() === -1){
                 sValueState = sap.ui.core.ValueState.Error;
             }else{
                 sValueState = sap.ui.core.ValueState.None;
             }
-/*
+
             this.getView().byId("idCheckBoxAtacado1").setValueState(sValueState);
             this.getView().byId("idCheckBoxVarejo1").setValueState(sValueState);
+
+            if(sValueState === sap.ui.core.ValueState.Error){
+                this.getView().byId("idCheckBoxAtacado1").focus();
+                MessageToast.show(this.getResourceBundle().getText("bandeira_obrig_txt"));
+                return 1;
+            }
 */
+
 /*
+            // Validação dos ToggleButton's "Bandeira"
+            if(this.getBandeiraSelectedIndex() === -1){
+                sValueState = sap.ui.core.ValueState.Error;
+            }else{
+                sValueState = sap.ui.core.ValueState.None;
+            }
+
             this.getView().byId("idTBAtacado1").setValueState(sValueState);
             this.getView().byId("idTBVarejo1").setValueState(sValueState);
-*/
 
             if(sValueState === sap.ui.core.ValueState.Error){
                 // this.getView().byId("idCheckBoxAtacado1").focus();
@@ -122,6 +142,7 @@ sap.ui.define([
                 MessageToast.show(this.getResourceBundle().getText("bandeira_obrig_txt"));
                 return 1;
             }
+*/
 
             // this.getOwnerComponent().getRouter().navTo("routeImprimirDetalhe");
             this.onImprimirDetalheOpen();
@@ -162,7 +183,7 @@ sap.ui.define([
                 this.getView().byId("idRadioButtonVisRelatLoja").setSelected(true);
                 // Somente Materiais CrossDocking
                 this.getView().byId("idCheckBoxSomenteMatXdock1").setSelected(false);
-/*
+
                 // Atacado
                 this.getView().byId("idCheckBoxAtacado1").setSelected(true);
                 // Varejo
@@ -171,7 +192,7 @@ sap.ui.define([
                 this.getView().byId("idCheckBoxTotUf1").setSelected(false);
                 // Totalizador por Grupo
                 this.getView().byId("idCheckBoxTotGrp1").setSelected(false);
-*/
+/*
                 // Atacado
                 this.getView().byId("idTBAtacado1").setPressed(true);
                 // Varejo
@@ -180,7 +201,7 @@ sap.ui.define([
                 this.getView().byId("idTBTotUf1").setPressed(false);
                 // Totalizador por Grupo
                 this.getView().byId("idTBTotGrp1").setPressed(false);
-
+*/
                 // Comprador (Clear)
                 this.byId("idMultiInputCompradorCod1").removeAllTokens();
                 // Contrato (Clear)
@@ -403,11 +424,12 @@ sap.ui.define([
 
 
         /**
-         * Identifica quais Toggle Buttons (descontinuado o CheckBox) de "Bandeira" foram selecionados
+         * Identifica quais Toggle Buttons (descontinuado o ToggleButton) de "Bandeira" foram selecionados
          * @public
          * @returns {number} valor de -1 à 2 identificando qual(is) CheckBox foi(ram) selecionado(s)
          */
         getBandeiraSelectedIndex: function(){
+/*
             if(this.getView().byId("idTBAtacado1").getPressed() && this.getView().byId("idTBVarejo1").getPressed()){
                 return 0;
             }else if(this.getView().byId("idTBAtacado1").getPressed()){
@@ -417,7 +439,7 @@ sap.ui.define([
             }else{
                 return -1;
             }
-/*
+*/
             if(this.getView().byId("idCheckBoxAtacado1").getSelected() && this.getView().byId("idCheckBoxVarejo1").getSelected()){
                 return 0;
             }else if(this.getView().byId("idCheckBoxAtacado1").getSelected()){
@@ -427,7 +449,6 @@ sap.ui.define([
             }else{
                 return -1;
             }
-*/
         },
 
 
@@ -492,10 +513,10 @@ sap.ui.define([
             let sPsvUf              = this.transformMultiInputIntoPsv("idMultiInputUf1");
             let iVisRelat           = this.getVisRelatSelectedIndex();
             let iBandeira           = this.getBandeiraSelectedIndex();
-            // let bTotalUf            = this.getView().byId("idCheckBoxTotUf1").getSelected();
-            // let bTotalGrupo         = this.getView().byId("idCheckBoxTotGrp1").getSelected();
-            let bTotalUf            = this.getView().byId("idTBTotUf1").getPressed();
-            let bTotalGrupo         = this.getView().byId("idTBTotGrp1").getPressed();
+            let bTotalUf            = this.getView().byId("idCheckBoxTotUf1").getSelected();
+            let bTotalGrupo         = this.getView().byId("idCheckBoxTotGrp1").getSelected();
+            // let bTotalUf            = this.getView().byId("idTBTotUf1").getPressed();
+            // let bTotalGrupo         = this.getView().byId("idTBTotGrp1").getPressed();
             let bXDocking           = this.getView().byId("idCheckBoxSomenteMatXdock1").getSelected();
 
             let oModel = this.getView().getModel();
@@ -519,7 +540,7 @@ sap.ui.define([
             sObjectPath += this.makeFilterPath("Mmsta", sPsvStatusMaterial, "eq", true);
             sObjectPath += this.makeFilterPath("UF", sPsvUf, "eq", true);
             sObjectPath += this.makeFilterPath("Visao", iVisRelat, "eq", true);
-            sObjectPath += this.makeFilterPath("Bandeira", iBandeira, "eq", true);
+            sObjectPath += this.makeFilterPath("Bandeira", iBandeira, "eq", true, true);
             sObjectPath += this.makeFilterPath("Total_UF", ((bTotalUf) ? "X" : ""), "eq", true, true);
             sObjectPath += this.makeFilterPath("Total_Grupo", ((bTotalGrupo) ? "X" : ""), "eq", true, true);
             sObjectPath += this.makeFilterPath("Cross", ((bXDocking) ? "X" : ""), "eq", true, true);
